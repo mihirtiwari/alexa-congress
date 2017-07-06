6331d011550f739765ffbef1a57f21b2d643774d
Mihir Tiwari <mtiw999@gmail.com>
2017-07-05 23:54:47 -0400

    needs testing and tweaking

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from flask_ask import Ask, question, statement
-import requests, json
+import requests, json, os
 
 app = Flask(__name__)
 ask = Ask(app, '/')
 
 MEMBERS_URL = "https://api.propublica.org/congress/v1/members/"
-API_KEY = ""
+API_KEY = os.environ['API_KEY']
 HEADERS = {"X-API-Key": API_KEY}
+
+SENATE_OR_HOUSE = 'senate'
 
 us_state_abbrev = {
     'Alabama': 'AL',
@@ -59,6 +61,7 @@ us_state_abbrev = {
     'Virginia': 'VA',
     'Washington': 'WA',
     'Washington, DC': 'DC',
+    'Washington DC': 'DC',
     'West Virginia': 'WV',
     'Wisconsin': 'WI',
     'Wyoming': 'WY',
@@ -69,12 +72,11 @@ def home():
     return 'Hello World!'
 
 def get_senators(postal_code):
-    # return array of senators' names
     url = MEMBERS_URL + "senate/" + postal_code + "/current.json"
     senators = {}
     req = requests.get(url, headers=HEADERS)
 
-    if req.status_code is not requests.code.ok:
+    if req.status_code is not 200:
         return []
 
     r = req.json()
@@ -85,6 +87,8 @@ def get_senators(postal_code):
         "I": "Independent"
     }
 
+    # handle Washington DC
+
     for result in r["results"]:
         name = result["name"]
         party = parties[result["party"]]
@@ -93,19 +97,64 @@ def get_senators(postal_code):
 
     return senators
 
+def get_house_reps(postal_code, district):
+    url = MEMBERS_URL + "house/" + postal_code + "/" + district + "/current.json"
+    house = {}
+    req = requests.get(url, headers=HEADERS)
+
+    if req.status_code is not 200:
+        return []
+
+    r = req.json()
+
+    parties = {
+        "D": "Democrat",
+        "R": "Republican",
+        "I": "Independent"
+    }
+
+    # handle Washington DC
+
+    for result in r["results"]:
+        name = result["name"]
+        party = parties[result["party"]]
+
+        house[name] = party
+
+    return house
+
+def get_num_reps(postal_code):
+    url = MEMBERS_URL + "house/" + postal_code + "/" + district + "/current.json"
+    house = {}
+    req = requests.get(url, headers=HEADERS)
+
+    if req.status_code is not 200:
+        return -1
+
+    r = req.json()
+
+    return len(r['results'])
+
 @ask.launch
 def start():
-    q = "Hello, what would you like to know about?"
+    q = "Hello, welcome to Congress Reps! For help, say \'Help\'. What would you like to know about our Congress?"
     return question(q)
 
 @ask.intent('SenateNoStateIntent')
 def senators_no_state():
     q = "What state would you like senators for?"
+
     return question(q)
 
 @ask.intent('SenateStateIntent')
 def senators_state(state):
-    if state not in us_state_abbrev.keys():
+    found = False
+    for s in us_state_abbrev.keys():
+        if state.lower() == s.lower():
+            state = s
+            found = True
+
+    if found is False:
         return statement("Invalid state given.")
 
     senators = get_senators(us_state_abbrev[state])
@@ -119,14 +168,100 @@ def senators_state(state):
 
     return statement(s)
 
+# figure out how to get district and state and all symantics
+@ask.intent('HouseNoStateIntent')
+def house_no_state():
+    q = "What state would you like representatives for?"
+
+    return question(q);
+
+@ask.intent('HouseNoDistrictIntent')
+def house_no_district():
+    q = "What district would you like representatives for?"
+
+    return question(q);
+
+@ask.intent('HouseStateDistrictSeparateIntent')
+def house_state_district_separate(district):
+    state = session.attribute['state']
+    if state is None:
+        return statement('Invalid request given.')
+
+    house_proper(state, district)
+
+@ask.intent('HouseProperIntent')
+def house_proper(state, district):
+    found = False
+    for s in us_state_abbrev.keys():
+        if state.lower() == s.lower():
+            state = s
+            found = True
+
+    if found is False:
+        return statement("Invalid state given.")
+
+    house = get_house_reps(us_state_abbrev[state], district)
+    if len(house) is 0:
+        return statement("Your request could not be processed.")
+
+    # print house reps
+    h = "Your representative is " + house.keys()[0] + " (" + house[house.keys()[0]] + ")"
+
+    return statement(h)
+
+@ask.intent('NumberofRepsTotalIntent')
+def num_reps():
+    return statement('Currently there are 435 representatives in the House of Representatives.')
+
+@ask.intent('NumberofRepsStateIntent')
+def num_reps_state(state):
+    found = False
+    for s in us_state_abbrev.keys():
+        if state.lower() == s.lower():
+            state = s
+            found = True
+
+    if found is False:
+        return statement("Invalid state given.")
+
+    num = get_num_reps(us_state_abbrev[state])
+
+    if num == -1:
+        return statement("Your request could not be processed.")
+
+    return statement('There are ' + str(num) + 'representatives in the state of ' + state)
+
+@ask.intent('NumberofSenatorsIntent')
+def num_senators():
+    return statement('Currently there are 100 senators in the Senate')
+
+@ask.intent('NumberofSenatorsStateIntent')
+def num_senators_state(state):
+    found = False
+    for s in us_state_abbrev.keys():
+        if state.lower() == s.lower():
+            state = s
+            found = True
+
+    if found is False:
+        return statement("Invalid state given.")
+
+    return statement('There are 2 senators in the state of ' + state)
+
+@ask.intent('AMAZON.HelpIntent')
+def help():
+    s = 'Thanks for using Congress Reps! To know a state\'s senators, say \'Who are the senators'
+    s += ' for\' and then the state name. To know a state\'s representatives by district, say '
+    s += '\'Who are the representatives for\' and then the state name and then \' for district\' '
+    s += 'and then the district number. To ask how many senators a state has, ask \'How many senators'
+    s += ' for\' and then the state\'s name. To ask how many representatives a state has, ask '
+    s += '\'How many representatives for\' and then the state\'s name.'
+
+    return statement(s);
+
 @ask.session_ended
 def session_ended():
     return "{}", 200
-# https://pythonprogramming.net/intro-alexa-skill-flask-ask-python-tutorial/
-# https://github.com/johnwheeler/flask-ask
-# https://www.youtube.com/watch?v=cXL8FDUag-s&list=PL6LVC9c1eflVHEbiNb0_dQF0LGLLrZtNL
-# https://developer.amazon.com/blogs/post/Tx14R0IYYGH3SKT/Flask-Ask-A-New-Python-Framework-for-Rapid-Alexa-Skills-Kit-Development
-# https://projects.propublica.org/api-docs/congress-api/endpoints/
 
 if __name__ == '__main__':
     app.run(debug=True)
