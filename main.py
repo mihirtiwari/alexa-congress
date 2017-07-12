83113f4529718e159e01386f97c672eb2763e62f
Mihir Tiwari <mtiw999@gmail.com>
2017-07-12 19:19:40 -0400

    trying to deploy to heroku

--- main.py
+++ main.py
@@ -283,7 +283,8 @@ def help():
 def session_ended():
     return "{}", 200
 
-# logging.getLogger('flask_ask').setLevel(logging.DEBUG)
+logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
